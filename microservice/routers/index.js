d852179c1c8251e54ac660883d69472f0f462737
NikhilsCode <nikhil.savant@eroam.com>
2024-09-19 00:46:00 +0530

    Deleted file

--- microservice/routers/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import authRouter from "./auth.js"
-import postRouter from "./post.js";
-const router =  Router();
-router.use("/auth", authRouter);
-router.use("/post", postRouter);
-
-
-export default router;
